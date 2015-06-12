e2d03bd12d7ac627c092913076b30293a83fb744
Jonathan Rowny <jrowny@gmail.com>
2015-06-12 03:43:16 -0400

    Update gridItem with new basedisplay

--- lib/GridItem.jsx
+++ lib/GridItem.jsx
@@ -1,16 +1,24 @@
 'use strict';
 
 import React from 'react';
+import BaseDisplayObject from './BaseDisplayObject.jsx';
 
-export default class GridItem extends React.Component {
+export default class GridItem extends BaseDisplayObject{
+
+  constructor() {
+    super();
+    this.onDrag = super.onDrag.bind(this);
+    this.onMouseOver = super.onMouseOver.bind(this);
+  }
+
   render() {
-    var itemStyle = this.props.style;
-    return <div style={itemStyle} className="gridItem">{this.props.item.name}</div>;
+    //IMPORTANT: Without the style, nothing happens :(
+    var itemStyle = super.getStyle.call(this);
+
+    return <div
+            onMouseDown={this.onDrag}
+            onMouseOver={this.onMouseOver}
+            style={itemStyle}
+            className="gridItem">{this.props.item.name}</div>;
   }
 }
-
-GridItem.propTypes = {
-  item: React.PropTypes.object,
-  style: React.PropTypes.object,
-  index: React.PropTypes.number
-};
