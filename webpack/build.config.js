e6362853ffc4811d47b59e0d2ae1c5e1d3d99982
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-11-12 10:02:47 +0100

    Added build task and webpack config to compile as UMD library. Added pre-publish task.

---
+++ webpack/build.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: './dist',
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'AbsoluteGrid'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
+      loader: 'babel' // The module to load. "babel" is short for "babel-loader"
+    }]
+  },
+  externals: {
+    react: 'React'
+  }
+};
+
+
